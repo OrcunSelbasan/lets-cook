b6a03e3b34533e41c2d94c9d0585d64424fc22cf
Orcun Selbasan <lutfuorcunselbasan@users.noreply.github.com>
2021-10-21 18:20:10 +0300

    Added search section

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,10 @@
 import {createHeroCategories} from "./components/Hero/HeroSectionCategories.js";
 import {randomCarouselRecipes, recipeEvent} from "./components/Hero/HeroSectionCarousel.js";
 import { createAreaDropdownOptions, displayRecipes } from "./components/Area/AreaSectionDropdown.js";
+import { search, clear } from "./components/Search/SearchBar.js";
 
+search();
+clear();
 createHeroCategories();
 randomCarouselRecipes();
 createAreaDropdownOptions();
---
+++ dist/js/components/Search/SearchBar.js
@@ -0,0 +1,137 @@
+import { fetchData } from "../Utils.js";
+
+const searchBtn = document.querySelector(".searchBtn");
+let searchInput = document.querySelector(".searchInput");
+const clearBtn = document.querySelector(".clearBtn");
+let searchResults = document.querySelector(".search-results");
+
+const search = () =>
+  searchBtn.addEventListener("click", (e) => {
+    let searchVal = searchInput.value;
+    if (searchVal !== "") {
+      if (searchVal.length > 1) {
+        fetchData(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchVal}`
+        )
+          .then((data) => {
+            if (searchResults.innerHTML.length > 0) {
+              searchResults.innerHTML = "";
+              data.meals.forEach((meal) => {
+                searchResults.innerHTML += `
+                                <div class="card m-3" style="width: 18rem;">
+                                    <img src="${meal["strMealThumb"]}" class="card-img-top mt-3 rounded-3" alt="">
+                                    <div class="card-body d-flex flex-column justify-content-between">
+                                      <h5 class="card-title text-center">${meal["strMeal"]}</h5>                        
+                                      <button class="btn btn-outline-primary" id="viewRecipe" data-bs-toggle="modal" data-bs-target="#exampleModal">View Recipe</button>
+                                    </div>
+                                </div>
+                             `;
+              });
+            } else {
+              data.meals.forEach((meal) => {
+                searchResults.innerHTML += `
+                                <div class="card m-3" style="width: 18rem;">
+                                    <img src="${meal["strMealThumb"]}" class="card-img-top mt-3 rounded-3" alt="">
+                                    <div class="card-body d-flex flex-column justify-content-between">
+                                      <h5 class="card-title text-center">${meal["strMeal"]}</h5>                        
+                                      <button class="btn btn-outline-primary" id="viewRecipe" data-bs-toggle="modal" data-bs-target="#exampleModal">View Recipe</button>
+                                    </div>
+                                </div>
+                             `;
+              });
+            }
+          })
+          .catch((e) => {
+            console.log(e.message);
+            searchResults.innerHTML =
+              "<p class='display-3 text-center'>Oops, Couldn't Find It</p>";
+            fetchData(
+              `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchVal[0]}`
+            )
+              .then((data) => {
+                document.querySelector(".error").innerContent =
+                  "Oops, Couldn't Find It";
+                if (searchResults.innerHTML.length > 0) {
+                  searchResults.innerHTML =
+                    "<p class='display-3 text-center'>Sorry, I couldn't find it :(. Maybe you like those</p>";
+                  data.meals.forEach((meal) => {
+                    searchResults.innerHTML += `
+                                      <div class="card m-3" style="width: 18rem;">
+                                          <img src="${meal["strMealThumb"]}" class="card-img-top mt-3 rounded-3" alt="">
+                                          <div class="card-body d-flex flex-column justify-content-between">
+                                            <h5 class="card-title text-center">${meal["strMeal"]}</h5>                        
+                                            <button class="btn btn-outline-primary" id="viewRecipe" data-bs-toggle="modal" data-bs-target="#exampleModal">View Recipe</button>
+                                          </div>
+                                      </div>
+                                   `;
+                  });
+                } else {
+                  data.meals.forEach((meal) => {
+                    searchResults.innerHTML += `
+                                      <div class="card m-3" style="width: 18rem;">
+                                          <img src="${meal["strMealThumb"]}" class="card-img-top mt-3 rounded-3" alt="">
+                                          <div class="card-body d-flex flex-column justify-content-between">
+                                            <h5 class="card-title text-center">${meal["strMeal"]}</h5>                        
+                                            <button class="btn btn-outline-primary" id="viewRecipe" data-bs-toggle="modal" data-bs-target="#exampleModal">View Recipe</button>
+                                          </div>
+                                      </div>
+                                   `;
+                  });
+                }
+              })
+              .catch((e) => {
+                console.log(e.message);
+                searchResults.innerHTML =
+                  "<p class='display-3 text-center'>Oops, Couldn't Find It</p>";
+              });
+          });
+      } else {
+        fetchData(
+          `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchVal}`
+        )
+          .then((data) => {
+            if (searchResults.innerHTML.length > 0) {
+              searchResults.innerHTML = "";
+              data.meals.forEach((meal) => {
+                searchResults.innerHTML += `
+                                <div class="card m-3" style="width: 18rem;">
+                                    <img src="${meal["strMealThumb"]}" class="card-img-top mt-3 rounded-3" alt="">
+                                    <div class="card-body d-flex flex-column justify-content-between">
+                                      <h5 class="card-title text-center">${meal["strMeal"]}</h5>                        
+                                      <button class="btn btn-outline-primary" id="viewRecipe" data-bs-toggle="modal" data-bs-target="#exampleModal">View Recipe</button>
+                                    </div>
+                                </div>
+                             `;
+              });
+            } else {
+              data.meals.forEach((meal) => {
+                searchResults.innerHTML += `
+                                <div class="card m-3" style="width: 18rem;">
+                                    <img src="${meal["strMealThumb"]}" class="card-img-top mt-3 rounded-3" alt="">
+                                    <div class="card-body d-flex flex-column justify-content-between">
+                                      <h5 class="card-title text-center">${meal["strMeal"]}</h5>                        
+                                      <button class="btn btn-outline-primary" id="viewRecipe" data-bs-toggle="modal" data-bs-target="#exampleModal">View Recipe</button>
+                                    </div>
+                                </div>
+                             `;
+              });
+            }
+          })
+          .catch((e) => {
+            console.log(e.message);
+            searchResults.innerHTML =
+              "<p class='display-3 text-center'>Oops, Couldn't Find It</p>";
+          });
+      }
+    }
+
+    console.log("executed");
+  });
+
+const clear = () => {
+    clearBtn.addEventListener("click", (e) => {
+        searchResults.innerHTML = "";
+        searchInput.value = "";
+    })
+}
+export { search, clear };
